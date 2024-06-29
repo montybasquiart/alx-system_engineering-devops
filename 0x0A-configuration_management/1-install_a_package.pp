0fefe86d218f14be0264943a9be1f41b70716ee6
Mathias Owa Martins <owamathias@gmail.com>
2024-06-29 03:26:55 +0100

    fixed code output error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # install flask from pip3
+
+package { 'python3-pip':
+    ensure => installed,
+}
+
 package { 'python3-flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require => Package['python3-pip'],
 }
