423de5f65fee9e1ebdfa299c84b6eb1ee593f9c8
Mathias Owa Martins <owamathias@gmail.com>
2024-07-01 04:12:00 +0100

    Lets practice puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 # Client configuration file
+include stdlib
+
 file_line { 'Turn off passwd auth': 
     ensure  => present,
     line    => '   PasswordAuthentication no',
     path    => '/etc/ssh/ssh_config',
-}
+    replace => true,}
 
 file_line { 'Declare identity file':
     ensure  => present,
     line    => '    IdentityFile ~/.ssh/school',
     path    => 'etc/ssh/ssh_config',
-}
+    replace => true,}
