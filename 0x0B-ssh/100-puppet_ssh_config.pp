9dd5144f2dda867a443693f198aaa09ccc69aa0b
Mathias Owa Martins <owamathias@gmail.com>
2024-07-09 11:39:06 +0100

    Fixed code output error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 # Client configuration file
-include stdlib
-
-file_line { 'Turn off passwd auth': 
-    ensure  => present,
-    line    => '   PasswordAuthentication no',
-    path    => '/etc/ssh/ssh_config',
-    replace => true,}
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+}
 
 file_line { 'Declare identity file':
-    ensure  => present,
-    line    => '    IdentityFile ~/.ssh/school',
-    path    => 'etc/ssh/ssh_config',
-    replace => true,}
+  ensure => present,
+  line   => '    IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
+}
