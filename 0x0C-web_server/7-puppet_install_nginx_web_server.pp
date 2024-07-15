dd0f4a10da50a01c67295d975869a716785422c7
Mathias Owa Martins <owamathias@gmail.com>
2024-07-15 10:57:14 +0100

    Configure server with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,20 @@
 # Puppet manifest to install nginx
 package { 'nginx':
-    ensure => installed,
+  ensure => installed,
 }
 
-file_line { 'rewrite':
-    ensure => 'present',
-    path   =>  '/etc/nginx/sites-available/default',
-    after  => 'listen 80 default_server;',
-    line   => 'rewrite ^/redirect_me$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-    content => 'Hello World!',
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-    ensure  => running,
-    require => package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
