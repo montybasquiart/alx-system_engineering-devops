ed1cbac38f0681e57f34416b31e36e79a562486f
Mathias Owa Martins <owamathias@gmail.com>
2024-07-19 02:07:30 +0100

    A custom HTTP header response with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,7 @@
 # Create a custom HTTP header response with puppet
 
 exec { 'HTTP header':
-        ensure => installed,
-        command => "sudo apt-get update -y;   
-        sudo apt-get install nginx -y;
-        sudo sed -i \"/server_name _;/a add_header X-Served-By '$HOSTNAME';\" /etc/nginx/sites-available/default;
-        sudo service nginx restart",
+        ensure   => installed,
+        command  => "sudo apt-get update -y && sudo apt-get install nginx -y && sudo sed -i \"/server_name _;/a add_header X-Served-By HOSTNAME;\" /etc/nginx/sites-available/default && sudo service nginx restart",
         provider => shell,
 }
