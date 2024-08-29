a8295947ffb0b6d7d1663ae8fe60ac488d2abfdd
montybasquiart <owamathias@gmail.com>
2024-08-29 02:01:16 +0100

    Fixed code output error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 """
-this doc for module
+Module to fetch the number of subscribers for a given subreddit.
 """
-import requests
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """method doc"""
+    """
+    Retrieves the number of subscribers for a given subreddit
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if an error occurs.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
+    try:
+        response = requests.get(url, allow_redirects=False, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get("data", {}).get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
