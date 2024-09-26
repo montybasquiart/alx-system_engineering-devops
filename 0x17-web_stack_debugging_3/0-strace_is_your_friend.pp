e09c89515be66f5aa45775bccbb3e177b253d317
Mathias Owa Martins <owamathias@gmail.com>
2024-09-26 03:24:50 +0100

    typo fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # a puppet code that fixes a Holberton Wordpress bug
 # 5xx error to 200 ok
-# edited the mistyped .phpp to .php in the /var/www/html/wp-settings.phpp file
+# edited the mistyped .phpp to .php in the /var/www/html/wp-settings.php file
 
 exec { 'fix-holberton-wordpress-server-error':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
