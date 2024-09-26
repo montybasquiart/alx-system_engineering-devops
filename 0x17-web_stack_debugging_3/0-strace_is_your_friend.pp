2b027c9b82d2b8f23e703ef18e131db51c9ae270
Mathias Owa Martins <owamathias@gmail.com>
2024-09-26 02:12:22 +0100

    Fixed code output wit strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
 # a puppet code that fixes a Holberton Wordpress bug
 # 5xx error to 200 ok
-# edited the mistyped .phpp to .php in the /var/www/html/wp-settings.php file
+# edited the mistyped .phpp to .php in the /var/www/html/wp-settings.phpp file
 
 exec { 'fix-holberton-wordpress-server-error':
-       command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-       path => '/usr/bin/:/bin',
+       command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.phpp',
+       path    => '/usr/bin/:/bin',
 }
